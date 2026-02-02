e54b17b4201c039ffa327e903fd121c9263684db
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-02 23:01:40 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#1a1a1a',
+        'darker-gray': '#0f0f0f',
+      },
+    },
+  },
+  plugins: [],
+}
